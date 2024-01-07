c3bdd3ad102648640da9bce458b2d88ea5d74fb7
unknown <minaster2312@gmail.com>
2024-01-07 19:12:30 +0100

    feat: slider animation

--- frontend/src/components/ImageSlider.tsx
+++ frontend/src/components/ImageSlider.tsx
@@ -8,11 +8,20 @@ export default function ImageSlider({ imageUrls }: ImageSliderProps) {
   const [imageIndex, setImageIndex] = useState(0);
 
   return (
-    <div className="w-96 h-96 relative">
-      <img
-        src={imageUrls[imageIndex]}
-        className="object-cover w-full h-full block box-border"
-      />
+    <div className="w-96 h-96 relative  overflow-hidden">
+    <div className={`w-full h-full transition-all flex`} style={{
+      translate: `${-100 * imageIndex}%`
+    }}>
+        {imageUrls.map((url) => {
+          return(
+            <img
+              key={url}
+              src={url}
+              className={`object-cover w-full h-full block box-border shrink-0 grow-0`}
+            />
+          )
+        })}
+      </div>
       <button
         className="absolute block top-0 bottom-0 p-4 cursor-pointer left-0"
         onClick={(e) => {
