f8635769bbb56b80039007a3e92b7022d6704d42
sit3kk <sito.270@interia.pl>
2024-04-27 15:10:07 +0200

    Fixed imports

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,9 @@ import botocore.exceptions as exceptions
 from dotenv import load_dotenv
 from pydantic import BaseModel
 import matplotlib.pyplot as plt
-from fastapi import FastAPI, HTTPException, UploadFile, CORSMiddleware
+from fastapi import FastAPI, HTTPException, UploadFile
+from starlette.middleware.cors import CORSMiddleware
+
 
 from facade import ModelFacade
 
@@ -18,11 +20,11 @@ from facade import ModelFacade
 app = FastAPI()
 
 app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 base = os.getcwd()
@@ -32,11 +34,11 @@ model.load_model(os.path.join(base, "models"), "100push0.7413.pth")
 
 load_dotenv()
 s3_client = boto3.client(
-    's3',
-    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
+    "s3",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
 )
-aws_bucket_name = os.getenv('AWS_BUCKET_NAME')
+aws_bucket_name = os.getenv("AWS_BUCKET_NAME")
 
 
 class Response(BaseModel):
@@ -64,8 +66,9 @@ async def upload(photo: UploadFile):
     """
     Uploads a photo and returns predicted species with corresponding confidence.
     """
-    dir_name = "".join(secrets.choice(
-        string.ascii_letters + string.digits) for _ in range(22)) + str(int(time.time()))
+    dir_name = "".join(
+        secrets.choice(string.ascii_letters + string.digits) for _ in range(22)
+    ) + str(int(time.time()))
 
     dir_path = os.path.join(base, "requests", dir_name)
     os.makedirs(dir_path)
@@ -73,7 +76,7 @@ async def upload(photo: UploadFile):
     img_path = os.path.join(dir_path, "upload.jpg")
     with open(img_path, "wb") as buffer:
         buffer.write(await photo.read())
-    
+
     try:
         img_tens = model.load_image(img_path)
     except FileNotFoundError:
@@ -82,31 +85,36 @@ async def upload(photo: UploadFile):
 
     predictions, img_original, prot_act, prot_act_pattern = model.predict(img_tens)
     plt.imsave(os.path.join(dir_path, "scaled.jpg"), img_original)
-    
+
     os.makedirs(os.path.join(dir_path, "activations"))
     activations = model.nearest_k_prototypes(
-        10, img_original, prot_act, prot_act_pattern)
+        10, img_original, prot_act, prot_act_pattern
+    )
 
     for i, img in enumerate(activations):
         plt.imsave(os.path.join(dir_path, "activations", f"{i}.jpg"), img)
 
     original_img_url = s3_upload_file(
-        os.path.join(dir_path, "upload.jpg"), os.path.join(dir_name, "upload.jpg"))
+        os.path.join(dir_path, "upload.jpg"), os.path.join(dir_name, "upload.jpg")
+    )
 
     scaled_img_url = s3_upload_file(
-        os.path.join(dir_path, "scaled.jpg"), os.path.join(dir_name, "scaled.jpg"))
+        os.path.join(dir_path, "scaled.jpg"), os.path.join(dir_name, "scaled.jpg")
+    )
 
     activation_urls = [
         s3_upload_file(
             os.path.join(dir_path, "activations", f"{i}.jpg"),
-            os.path.join(dir_name, "activations", f"{i}.jpg")) for i in range(10)
+            os.path.join(dir_name, "activations", f"{i}.jpg"),
+        )
+        for i in range(10)
     ]
 
     shutil.rmtree(os.path.join(dir_path))
-   
+
     return Response(
         predictions=predictions,
         original_img_url=original_img_url,
         scaled_img_url=scaled_img_url,
-        activation_urls=activation_urls
+        activation_urls=activation_urls,
     )
