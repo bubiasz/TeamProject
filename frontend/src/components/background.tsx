76b891e1bedfd62b3a7752a017a89a417b3cd302
sit3kk <sito.270@interia.pl>
2024-04-28 08:13:50 +0200

    Bubbles background

--- frontend/src/components/background.tsx
+++ frontend/src/components/background.tsx
@@ -9,7 +9,120 @@ export const Background = (): JSX.Element => {
   }, []);
 
   const options = {
-    preset: "links",
+    background: {
+      color: {
+        value: "#000",
+      },
+    },
+    fpsLimit: 120,
+    interactivity: {
+      detectsOn: "canvas" as "canvas",
+      events: {
+        onHover: {
+          enable: true,
+          mode: "bubble"
+        },
+        onClick: {
+          enable: true,
+          mode: "repulse"
+        },
+        resize: true
+      },
+      modes: {
+        grab: {
+          distance: 400,
+          lineLinked: {
+            opacity: 0.5
+          }
+        },
+        bubble: {
+          distance: 400,
+          size: 4,
+          duration: 0.3,
+          opacity: 0.5,
+          speed: 3
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4
+        },
+        push: {
+          particlesNb: 4
+        },
+        remove: {
+          particlesNb: 2
+        }
+      }
+    },
+    particles: {
+      color: {
+        value: "#BF40BF",
+      },
+      number: {
+        value: 400,
+        density: {
+          enable: true,
+          area: 800
+        }
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000"
+        },
+        polygon: {
+          nbSides: 5
+        },
+        image: {
+          src: "img/github.svg",
+          width: 100,
+          height: 100
+        }
+      },
+      opacity: {
+        value: 0.4,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 1,
+          opacityMin: 0.1,
+          sync: false
+        }
+      },
+      size: {
+        value: 10,
+        random: true,
+        anim: {
+          enable: false,
+          speed: 40,
+          sizeMin: 0.1,
+          sync: false
+        }
+      },
+      lineLinked: {
+        enable: false,
+        distance: 500,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 2
+      },
+      move: {
+        enable: true,
+        speed: 0.5,
+        direction: "bottom",
+        random: false,
+        straight: false,
+        outMode: "out",
+        bounce: false,
+        attract: {
+          enable: false,
+          rotateX: 600,
+          rotateY: 1200
+        }
+      }
+    },
+    detectRetina: true
   };
 
   return (
@@ -18,52 +131,15 @@ export const Background = (): JSX.Element => {
         id="tsparticles"
         init={customInit}
         options={{
-          background: {
-            color: {
-              value: "#000",
-            },
-          },
-          fpsLimit: 120,
-          interactivity: {},
+          ...options,
           particles: {
-            color: {
-              value: "#ffffff",
-            },
-            links: {
-              color: "#BF40BF",
-              distance: 150,
-              enable: true,
-              opacity: 0.5,
-              width: 1,
-            },
+            ...options.particles,
             move: {
-              direction: "none",
-              enable: true,
-              outModes: {
-                default: "bounce",
-              },
-              random: false,
-              speed: 2,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 120,
-            },
-            opacity: {
-              value: 0.5,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              value: { min: 0.2, max: 2 },
-            },
-          },
-          detectRetina: true,
+              ...options.particles.move,
+              direction: "none", 
+              outMode: "bounce" 
+            }
+          }
         }}
       />
     </div>
